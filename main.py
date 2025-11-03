d9e0c423fde74e3845b42a654db043ab5dd543e5
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-11-03 10:35:25 -0800

    need to figure out what is going on with the "s" showing up

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getForumHome():
+def get_forum_home():
     request = requests.get("https://scratch.mit.edu/discuss", verify=False) #because of the stupid BYOD
     req_text = request.text
     soup = BeautifulSoup(req_text, features="html.parser")
@@ -21,13 +21,10 @@ def getForumHome():
             forum_name = forum_tr.select_one("tr .tcl .intd .tclcon h3").text.split("\n")[0]
             forum_id = forum_tr.select_one("tr .tcl .intd .tclcon h3 a").get("href").split("/discuss/")[1].split("/")[0]
             forum_desc_tag = forum_tr.select_one("tr .tcl .intd .tclcon")
-            print("oegoirg")
-            print(forum_desc_tag)
             try:
                 forum_desc = forum_desc_tag.contents[-1].text.split("\n                ")[1]
             except IndexError:
                 forum_desc = ""
-            print(forum_desc)
             topic_count = forum_tr.select_one("tr .tc2").text
             post_count = forum_tr.select_one("tr .tc3").text
             forum_dict = {
@@ -39,6 +36,16 @@ def getForumHome():
             }
             category_dict["forums"].append(forum_dict)
         categories.append(category_dict)
-    print(categories)
+    return categories
 
+def print_forum_info(categories: list):
+    print("Discussion Forums Home")
+    for category in categories:
+        testing = not len(category["forums"]) == 1 and "s"
+        print(f"=== {category["category_name"]} === ({len(category["forums"])} forum{testing})")
+        for forum in category["forums"]:
+            print(f"> {forum["title"]} ({forum["topic_count"]} topics, {forum["post_count"]} posts) (ID {forum["id"]})")
+            print(forum["description"])
 
+categories = get_forum_home()
+print_forum_info(categories)
